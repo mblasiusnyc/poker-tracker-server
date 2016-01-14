a19b6d12d96c7871049359b7548f625ba7156009
Michael Blasius <mblasiusnyc@gmail.com>
2016-01-14 12:36:45 -0700

    Removed favicon refs

--- server/routes.js
+++ server/routes.js
@@ -20,12 +20,12 @@ module.exports = function(app) {
   	.get(errors[404]);
 
   // All other routes should redirect to the index.html
-  app.route('/sessions')
-    .get(function(req, res) {
-      res.sendFile(path.resolve(app.get('appPath') + '/index.html'));
-    });
+  // app.route('/sessions')
+  //   .get(function(req, res) {
+  //     res.sendFile(path.resolve(app.get('appPath') + '/index.html'));
+  //   });
 
-  app.all('*', function(req, res){
-  	res.redirect('/sessions')
-  });
+  // app.all('*', function(req, res){
+  // 	res.redirect('/sessions')
+  // });
 };
